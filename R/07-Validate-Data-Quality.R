c9bcb80f6dad419b708c0790b59a02768da3ca92
Christian Maino Vieytes <cam17@illinois.edu>
2022-07-07 08:45:47 -0700

    Delete 07-Validate-Data-Quality.R

--- R/07-Validate-Data-Quality.R
+++
@@ -1,9 +0,0 @@
-###---------------------------------------------------
-###   07-VALIDATE DATA QUALITY
-###---------------------------------------------------
-
-library( tidyverse )
-
-# In this script we will run a series of analysis to test the validity of our assumptions for this dataset and assess the 
-# scope of its representativeness.
-
