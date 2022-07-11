61e8b4e84009685dfc3ab5fa1b039ca5a08f5543
Christian Maino Vieytes <cam17@illinois.edu>
2022-07-11 12:00:04 -0700

    sidebar layout and reactive input

--- R/Nonprofit-Density/app.R
+++ R/Nonprofit-Density/app.R
@@ -14,6 +14,7 @@ library( bslib )
 library( shinydashboard )
 library( sf )
 library( shinythemes )
+library( shinyWidgets )
 
 
 # Import counties landing page map
@@ -27,48 +28,64 @@ cnties.dorling <- readRDS( "Dashboard-County-Data/Dorling-Shapefiles/USA-Countie
 
 # merge
 
-lp.1 <- ggplot( ) + geom_sf( cnties,
-                     mapping = aes( fill = dens ),
-                     color = NA, size = 0.5 )+
+lp.plot.chloro <- function( df ){
+  ggplot( ) + geom_sf( df,
+                             mapping = aes( fill = dens ),
+                             color = NA, size = 0.5 )+
   theme_minimal( ) +
-theme( legend.position = 'none' )
+  theme( legend.position = 'none' )
+}
 
-  
 
-lp.2 <- ggplot(  )  +
-  geom_sf( cnties.dorling, mapping = aes( fill = dens ),  color = NA ) +
+lp.plot.dorling <- function( df ){
+  ggplot(  )  +
+  geom_sf( df, mapping = aes( fill = dens ),  color = NA ) +
   theme_minimal()
-  
+}
+
+# USER INTERFACE
 ui <- bootstrapPage(
   
-    navbarPage(theme = shinytheme( "flatly" ), collapsible = TRUE,
-               HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">U.S. Nonprofit Density</a>'), 
-               id="nav",
-               tabPanel("New Nonprofit Cumulative Density, 2014-2021 : U.S. Counties",
-                        
-                          
-                          mainPanel(
-                            tabsetPanel(
-                              tabPanel("Chloropleth", plotOutput("lp.1")),
-                              tabPanel("Dorling", plotOutput("lp.2") )
-                            )
-                          )
-               )
-    )
+  navbarPage(theme = shinytheme( "flatly" ), collapsible = TRUE,
+             HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">U.S. Nonprofit Density</a>'), 
+             id="nav",
+             tabPanel("New Nonprofit Cumulative Density, 2014-2021 : U.S. Counties",
+                      
+                      sidebarLayout(
+                        sidebarPanel(
+                          pickerInput("yr_select", "Year:",   
+                                      choices = c("Cumulative: 2014-2021"), 
+                                      selected = c("Cumulative: 2014-2021"),
+                                      multiple = FALSE) ) ,
+                      
+                      mainPanel(
+                        tabsetPanel(
+                          tabPanel("Chloropleth", plotOutput("lp.1")))
+                        )
+                      )
+             )
+  )
 )
 
-                                         
+  
 
-
-
+# SERVER
 server <- function( input, output ) { 
   
-  output$lp.1 <- renderPlot( lp.1 )
-  
-  output$lp.2 <- renderPlot( lp.2 )
   
   
+  year.reactive.df <- reactive( if(input$yr_select=="Cumulative: 2014-2021") {
+    cnties
   }
+  
+  
+  )
+
+  output$lp.1 <- renderPlot( lp.plot.chloro( year.reactive.df() ) )
+  
+
+}
+
 
 shinyApp( ui, server )
 
