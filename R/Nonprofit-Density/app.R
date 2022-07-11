a155f6c2ff5506c58812938d36b5c3fbd8c7dae5
Christian Maino Vieytes <cam17@illinois.edu>
2022-07-11 11:08:20 -0700

    functioning landing page basic

--- R/Nonprofit-Density/app.R
+++ R/Nonprofit-Density/app.R
@@ -13,29 +13,61 @@ library( plotly )
 library( bslib )
 library( shinydashboard )
 library( sf )
+library( shinythemes )
 
-ui <- dashboardPage(
+
+# Import counties landing page map
+main <- "/Volumes/My Passport for Mac/Urban Institute/Summer Projects/Geospatial Dashboard/"
+
+setwd( paste0( main, "np-density-dashboard/Data-Rodeo" ) )
+
+
+cnties <- readRDS( "Dashboard-County-Data/USA-Counties.rds" )
+cnties.dorling <- readRDS( "Dashboard-County-Data/Dorling-Shapefiles/USA-Counties-Dorling.rds" )
+
+# merge
+
+lp.1 <- ggplot( ) + geom_sf( cnties,
+                     mapping = aes( fill = dens ),
+                     color = NA, size = 0.5 )+
+  theme_minimal( ) +
+theme( legend.position = 'none' )
+
   
-  dashboardHeader(
-    title = "Nonprofit Density"
-  ),
+
+lp.2 <- ggplot(  )  +
+  geom_sf( cnties.dorling, mapping = aes( fill = dens ),  color = NA ) +
+  theme_minimal()
   
-  dashboardSidebar(),
+ui <- bootstrapPage(
   
-  dashboardBody(
-    
-    fluidRow(
-      box( plotOutput("plot1", height = 250 ) ),
-      
-      box(
-        title = "Controls",
-        sliderInput( "slider", "Number of observations:", 1, 100, 50 ) )
+    navbarPage(theme = shinytheme( "flatly" ), collapsible = TRUE,
+               HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">U.S. Nonprofit Density</a>'), 
+               id="nav",
+               tabPanel("New Nonprofit Cumulative Density, 2014-2021 : U.S. Counties",
+                        
+                          
+                          mainPanel(
+                            tabsetPanel(
+                              tabPanel("Chloropleth", plotOutput("lp.1")),
+                              tabPanel("Dorling", plotOutput("lp.2") )
+                            )
+                          )
+               )
     )
-  )
 )
+
+                                         
+
 
 
 server <- function( input, output ) { 
+  
+  output$lp.1 <- renderPlot( lp.1 )
+  
+  output$lp.2 <- renderPlot( lp.2 )
+  
+  
   }
 
 shinyApp( ui, server )
