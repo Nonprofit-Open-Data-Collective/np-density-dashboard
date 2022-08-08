0525653a5580bf7af3596f0cbdfe02fd4c7bee9e
Christian Maino Vieytes <cam17@illinois.edu>
2022-08-08 12:03:24 -0400

    basic plot generated

--- R/10-Shapefiles-for-Boardmember-Maps.R
+++ R/10-Shapefiles-for-Boardmember-Maps.R
@@ -16,6 +16,7 @@
 library( tidyverse )
 library( sf )               # simple features framework     
 library( geosphere )
+library( data.table )       # for manipulating data for plotting BM to NPO
 source('/Volumes/My Passport for Mac/Urban Institute/Summer Projects/Geospatial Dashboard/np-density-dashboard/R/helpers.R')
 
 
@@ -80,7 +81,8 @@ nrow( npo.keep )
 ppl.keep <- ppl %>%
   select( Case.Number, lat, lon ) %>%
   group_by( Case.Number ) %>%
-  mutate( bm = paste0( "BM.", row_number() ) ) %>% # create board member identifier within an NPO
+  mutate( bm = row_number() ) %>% # create board member identifier within an NPO
+  mutate( bm = ifelse( bm < 10, paste0( "BM.0", bm ), paste0( "BM.", bm ) ) ) %>%  # add leading zeros
   ungroup() %>%
   arrange( Case.Number )
 
@@ -100,7 +102,7 @@ npo.ppl <- left_join( npo.distinct, ppl.wide )
 
 # test the function using NPO location and BM 1 coordinates
 mypts.npo <- data.frame( npo.ppl$lon.npo,npo.ppl$lat.npo )    # lon goes first and lat goes second in the data.frame
-mypts.bm1<- data.frame( npo.ppl$lon.BM.1, npo.ppl$lat.BM.1 )
+mypts.bm1<- data.frame( npo.ppl$lon.BM.01, npo.ppl$lat.BM.01 )
 
 
 dis.miles <- distHaversine( p1 = mypts.npo,
@@ -115,7 +117,7 @@ dis.meters <- distHaversine( p1 = mypts.npo,
 # ensure this is run before loop:
 mypts.npo <- data.frame( npo.ppl$lon.npo,npo.ppl$lat.npo )    
 
-
+bm.tails <- c( paste0( "0", 1:9 ), 10 )
 for( i in 1:10){
   
   
@@ -123,13 +125,13 @@ for( i in 1:10){
   
   
   # select correct columns for respective board member
-  mypts.bm <- data.frame( eval( parse( text = ( paste0( "npo.ppl$lon.BM.", i ) ) ) ), 
-                          eval( parse( text = ( paste0( "npo.ppl$lat.BM.", i ) ) ) ) )
+  mypts.bm <- data.frame( eval( parse( text = ( paste0( "npo.ppl$lon.BM.", bm.tails[i] ) ) ) ), 
+                          eval( parse( text = ( paste0( "npo.ppl$lat.BM.", bm.tails[i] ) ) ) ) )
   
-  npo.ppl[[ paste0( "dist.BM.", i, ".miles" ) ]] <- distHaversine( p1 = mypts.npo,
+  npo.ppl[[ paste0( "dist.BM.", bm.tails[i], ".miles" ) ]] <- distHaversine( p1 = mypts.npo,
                               p2 = mypts.bm1 )*0.0006213711 # convert default meters to miles
   
-  npo.ppl[[ paste0( "dist.BM.", i, ".meters" ) ]] <- distHaversine( p1 = mypts.npo,
+  npo.ppl[[ paste0( "dist.BM.", bm.tails[i], ".meters" ) ]] <- distHaversine( p1 = mypts.npo,
                                                                             p2 = mypts.bm1 )
   
   
@@ -143,5 +145,61 @@ for( i in 1:10){
 }
 
 
+sum(is.na(npo.ppl$lon.npo)) # 167 missing coordinates
+sum(is.na(npo.ppl$lat.npo))
 
+# remove NAs otherwise st_as_sf can't generate sf file
+# put data back into long format for sf (geometries will be lines for each row
+# representing a line between the NPO and the BM coordinates)
+no.na <- npo.ppl%>%
+  filter( !is.na( lon.npo ) ) %>%
+  mutate( bm = paste0( "BM.", bm.tails[i] ),
+          id = row_number() ) %>%
+  select( GEOID,
+          Case.Number,
+          id,
+          bm,
+          ORGNAME,
+          Nteecode,
+          YR,
+          contains( paste0( "lon.BM.", bm.tails[i] ) ), 
+          contains( paste0( "lat.BM.", bm.tails[i] ) ),
+          lon.npo,
+          lat.npo )
+
+# generate sf file
+
+
+# `data.table` way: This code was modified from the reproducible example online:
+# https://newbedev.com/r-create-linestring-from-two-points-in-same-row-in-dataframe
+
+dt <- as.data.table(no.na)
+
+dt1 <- dt[, .(id, lon = lon.BM.01, lat = lat.BM.01)]
+dt2 <- dt[, .(id, lon = lon.npo, lat = lat.npo)]
+
+## Add on a 'sequence' variable so we know which one comes first
+dt1[, seq := 1L ]
+dt2[, seq := 2L ]
+
+## put back together
+dt <- rbindlist(list(dt1, dt2), use.names = TRUE)
+setorder(dt, id, seq)
+
+sf <- sfheaders::sf_linestring(
+  obj = dt
+  , x = "lon"
+  , y = "lat"
+  , linestring_id = "id"
+)
+
+
+out.line.sf <- st_as_sf( left_join( bind.dt, sf, by = "id" ) )
+
+cn.lev <- levels( as.factor(out.line.sf$Case.Number ) )
+(d <- out.line.sf %>% filter( Case.Number == cn.lev[109] ))
+ggplot( )+
+  geom_sf( data = st_geometry(d), lwd = 1, lineend = "round" ) 
+
+plot(st_geometry(d), lwd = 1, lineend = "round")
 
